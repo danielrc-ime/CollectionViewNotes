abe338a4efcd1ad1dedc4ade9c13c12cecdaa14a
daniel_master <daniel.rodriguez@master.com.mx>
2020-09-11 18:33:49 -0500

    init

---
+++ CollectionViewNotes/Controller/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewNotes
+//
+//  Created by MacInnovacion on 11/09/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
